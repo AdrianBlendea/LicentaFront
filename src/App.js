ca5d1c3fa3964dfa46a1d712db361dff1c5a6f8f
adrian.blendea <adrianblendea@accesa.eu>
2024-06-23 12:16:46 +0300

    login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import SignInForm from './components/login/SignIn';
 
 
 import HomePage from './components/HomePage';
@@ -8,6 +9,7 @@ import HomePage from './components/HomePage';
 import EditorPage from './components/EditorPage';
 import DocumentPage from './components/DocumentPage/DocumentPage';
 import ResponsiveAppBar from './components/ResponsiveAppBar';
+import LoginPage from './components/login/LoginPage';
 
 function App() {
   return (
@@ -22,6 +24,7 @@ function App() {
         </nav>
         <Routes>
           <Route path="/home" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage/>} />
           <Route path="/editor" element={<EditorPage />} />
           <Route path="/documents" element={<DocumentPage />} />
           <Route path="/editor/:problemId" element={<EditorPage />} />
